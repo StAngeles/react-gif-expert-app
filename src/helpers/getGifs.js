eb3f3ba9e069abf0cf33ca3ae8e283ecbb6d0b8a
Edgar Yahir <edgaryahir.st@gmail.com>
2022-02-02 13:54:13 -0600

    first commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+    category
+  )}&limit=10&api_key=r5Ak0fT5oCLVlX75K5G4O0fakYV8CLtn`;
+  const respuesta = await fetch(url);
+  const { data } = await respuesta.json();
+
+  const gifs = data.map((img) => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url,
+    };
+  });
+
+  return gifs;
+};
